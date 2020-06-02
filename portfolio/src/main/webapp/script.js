f9b36febc8d9e3dfe42dc936b6bffe0e6659b55b
Lina Montes <monteslina54@gmail.com>
2020-06-02 17:07:49 +0000

    Add asynchronous JS function to fetch data from server and add to page

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -36,3 +36,9 @@ function addRandomFunFact() {
     const funFactContainer = document.getElementById('fun-fact-container');
     funFactContainer.innerText = funFact;
 }
+
+async function addContent() {
+  const response = await fetch('/data');
+  const hello = await response.text();
+  document.getElementById('data').innerHTML = hello;
+}
