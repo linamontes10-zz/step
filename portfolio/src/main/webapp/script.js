f6e25717b821a67ab4be714ed3431eb1fb9a0f5b
Lina Montes <monteslina54@gmail.com>
2020-06-09 14:17:27 +0000

    Add JS function to format sentiment of comments with a gif

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -15,12 +15,6 @@
 /**
  * Adds a random fun fact to the page.
  */
-
-// Spotify API keys to be used for later
-// var client_id = '444dda318d8949cb9282d95a9885a7c6';
-// var client_secret = '9f81ba6cf5748ef9d24a7ddb1e8bc4c';
-// var redirect_uri = 'https://8080-dot-12317386-dot-devshell.appspot.com/misc.html';
-
 function addRandomFunFact() {
     const funFacts =
       ['I am a Capricorn sun, Aquarius Moon, and Cancer rising.',
@@ -38,21 +32,41 @@ function addRandomFunFact() {
 }
 
 /**
+ * Formats comments with sentiment.
+ */
+function formatSentimentComments(commentsJson) {
+
+  for (comment in commentsJson.comments) {
+    let sentimentScore = commentsJson.scores[comment];
+    let gif = document.createElement('img');
+
+    if (sentimentScore > 0.4) {
+      gif.setAttribute('src', '/images/happy.gif');
+    } else if (sentimentScore < -0.4) {
+      gif.setAttribute('src', '/images/sad.gif');
+    } else {
+      gif.setAttribute('src', '/images/neutral.gif');
+    }
+    document.getElementById('comments-container').innerText +=
+        commentsJson.comments[comment] + "\n\n";
+    document.getElementById('comments-container').appendChild(gif);
+  }
+}
+
+/**
  * Fetches comments from the server and adds them to the DOM.
  */
 async function displayComments() {
   const commentLimit = document.getElementById("comment-limit").value;
   const response = await fetch(`/add-comments?comment-limit=${commentLimit}`);
-  const comments = await response.json();
-  let commentsResult;
+  const commentsJson = await response.json();
 
-  if (comments.length) {
-    commentsResult = comments.join('\n\n');
+  if (commentsJson.length) {
+    formatSentimentComments(commentsJson);
   } else {
-    commentsResult = "Uh-oh, there are no comments to display! You can add one below.";
+    document.getElementById('comments-container').innerText =
+        "Uh-oh, there are no comments to display! You can add one below.";
   }
-
-  document.getElementById('comments-container').innerText = commentsResult;
 }
 
 /**
